8290dfc4ab137a814304288a320514d82dca7b29
Rahul pratap singh <47356284+rpsingh11@users.noreply.github.com>
2021-02-14 14:13:04 +0530

    add next and previous

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Questions from './Questions'
+import Footer from './Footer'
 const App = () =>{
 	return(
 		<div>
 		<Header />
 		<Questions />
 		</div>
-		)
+		) 
 }
 
 export default App;
--- src/containers/Questions.jsx
+++ src/containers/Questions.jsx
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import QuestionNo from './QuestionNo';
-import Qs from '../Questions';
-const Questions=()=>{
+import questions_data from '../Data/questions_data';
+import Footer from './Footer'
 
+const Questions=()=>{
+	const [questionNumber, setQuestionNumber] = useState(0);
+	var showNext = true;
+	const nextQuestion = () =>{
+		console.log('questionNumber', questionNumber);
+		if(questionNumber === 8){
+			showNext= false;
+		}
+		setQuestionNumber(questionNumber+1);
+	}
+	const prevQuestion = () =>{
+		setQuestionNumber(questionNumber-1);
+	}
+	const {que, optA, optB, optC, optD} = questions_data[questionNumber];
 	return(
+		<div>
 		<div className='container'>
 		<div className='questionandNumber'>
 			<div className='questions'>
-			Q1.What is the Full Form of CSS?
-			{Qs[0].Question}
+			Q1.{que}
 			</div>
 			<div className='options'>
 			<form method='post'>
-			<h3><input type="radio" name="option" value='xyz'/>
-			Cascade style Standard</h3>
-			<h3><input type="radio" name="option" />Cascade style Sheet</h3>
-			<h3><input type="radio" name="option" />Cascade Stylish Sheet</h3>
-			<h3><input type="radio" name="option" />Common style Standard</h3>
+			<h3><input type="radio" name="option"/>{optA}</h3>
+			<h3><input type="radio" name="option" />{optB}</h3>
+			<h3><input type="radio" name="option" />{optC}</h3>
+			<h3><input type="radio" name="option" />{optD}</h3>
 			</form>
 			</div>
 			</div>
 			<div className='questionNo'>
 				<QuestionNo />
 			</div>
+			</div>
+			<Footer next={nextQuestion} prev={prevQuestion} display = {questionNumber}/>
 		</div>
 		)
 }
