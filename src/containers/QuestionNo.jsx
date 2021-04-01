eb2d70bff2e8537fe1b7933aa5cafb5ca11e13c9
Sachin Rana <sachinrana1827@gmail.com>
2021-04-01 19:48:11 +0530

    done

--- src/containers/QuestionNo.jsx
+++ src/containers/QuestionNo.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import lodash from 'lodash';
-const QuestionNo = (props) =>{
-			let card = [];
-lodash.times(50, (i) => {
-  card.push(<button name={i} id={props.id}>1</button>);
-});
-	return(
-	<div >
-		{card}
-		<button>Submit</button>
-		</div>
-		)
-}
+import React from "react";
+import lodash from "lodash";
+const QuestionNo = () => {
+  let card = [];
+  lodash.times(10, i => {
+    card.push(<button key={i}>{i + 1}</button>);
+  });
+  return (
+    <div>
+      {card}
+      <br />
+      <button>Submit</button>
+    </div>
+  );
+};
 
-
-export default QuestionNo
+export default QuestionNo;
